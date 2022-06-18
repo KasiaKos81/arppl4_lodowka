4e9b2e6e09042f81274711bd38e762d63d1855fb
Kasia <kasiakos81@gmail.com>
2022-06-18 10:03:53 +0200

    commit 1806

--- src/main/java/Lodowka.java
+++ src/main/java/Lodowka.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 public class Lodowka {
 
-    private Map<String, Skladnik> skladniki = new HashMap<>();
+    private Map<String, Skladnik> mapaSkladniki = new HashMap<>();
 
     public void dodajSkladnik(String nazwa, IloscSkladnika ilosc, double limitOstrzezenie, double limitKrytyczny) {
-        if (!skladniki.containsKey(nazwa)) {
-            skladniki.put(nazwa, new Skladnik(nazwa, ilosc, limitOstrzezenie, limitKrytyczny));
+        if (!mapaSkladniki.containsKey(nazwa)) {
+            mapaSkladniki.put(nazwa, new Skladnik(nazwa, ilosc, limitOstrzezenie, limitKrytyczny));
         }
     }
     public List<Skladnik> zwrocSkladnikiKtorychJestMalo(){
         List<Skladnik> listaSkladnikowKtorychJestMalo = new ArrayList<>();
-        for (Skladnik skladnik : skladniki.values()) {
+        for (Skladnik skladnik : mapaSkladniki.values()) {
             if(skladnik.getLimitOstrzezenie() <= 3.0){
                 listaSkladnikowKtorychJestMalo.add(skladnik);
             }
@@ -19,13 +19,21 @@ public class Lodowka {
     }
     public List<Skladnik> zwrocSkladnikiKtorychJestWCholereMalo(){
         List<Skladnik> listaSkladnikowKtorychJestWCholereMalo = new ArrayList<>();
-        for (Skladnik skladnik : skladniki.values()) {
+        for (Skladnik skladnik : mapaSkladniki.values()) {
             if(skladnik.getLimitKrytyczny() <= 2.0){
                 listaSkladnikowKtorychJestWCholereMalo.add(skladnik);
             }
         } return listaSkladnikowKtorychJestWCholereMalo;
     }
+    public void zwiekszIloscSkladnika(String nazwa, double ilosc){
+        if(!mapaSkladniki.containsKey(nazwa)){
+            System.out.println("Nie ma takiego składnika");
+            return;
+        }
+        Skladnik skladnik  = mapaSkladniki.get(nazwa);
+        skladnik.setIlosc((skladnik.getIlosc()) + ilosc);
 
+    }
 
 
     }
