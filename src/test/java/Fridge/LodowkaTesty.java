c6862ac6a08ce44860865305e11ef5383f5bcbd1
Kasia <kasiakos81@gmail.com>
2022-06-19 11:25:48 +0200

    commit 190622

--- src/test/java/Fridge/LodowkaTesty.java
+++ src/test/java/Fridge/LodowkaTesty.java
@@ -13,12 +13,12 @@ public class LodowkaTesty {
 
     @Test
     public void test_mozliweJestDodawanieSkladnika() {
-        Skladnik testowanySkladnik = new Skladnik("Jablko",new IloscSkladnika(6.0, Jednostka.SZTUKA) , 5.0, 3.0);
+        Skladnik testowanySkladnik = new Skladnik("Jablko", new IloscSkladnika(6.0, Jednostka.SZTUKA) , 5.0, 3.0);
         Lodowka lodowka = new Lodowka();
         lodowka.dodajSkladnik(testowanySkladnik.getNazwa(), testowanySkladnik.getIloscS().getJednostka(), testowanySkladnik.getLimitOstrzezenie(), testowanySkladnik.getLimitKrytyczny());
         List<Skladnik> wynikZwroconaLista = lodowka.zwrocListeSkladnikow();
         Assert.assertTrue("lista zwrocona zawiera skladnik", wynikZwroconaLista.contains(testowanySkladnik));
-        Assert.assertEquals("lista pwoinna zawierac dodane skladniki", 1.0, wynikZwroconaLista.size());
+        Assert.assertEquals("lista powinna zawierac dodane skladniki", 1, wynikZwroconaLista.size());
     }
 
 
