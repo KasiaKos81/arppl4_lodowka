5ea476c49f3aab509a0f36614ec1a83777d200f9
Kasia <kasiakos81@gmail.com>
2022-06-22 00:17:07 +0200

    210622

--- src/test/java/Fridge/LodowkaTesty.java
+++ src/test/java/Fridge/LodowkaTesty.java
@@ -13,12 +13,31 @@ public class LodowkaTesty {
 
     @Test
     public void test_mozliweJestDodawanieSkladnika() {
-        Skladnik testowanySkladnik = new Skladnik("Jablko", new IloscSkladnika(6.0, Jednostka.SZTUKA), 5.0, 3.0);
+        Skladnik testowanySkladnik = new Skladnik("Jablko", new IloscSkladnika(0.0, Jednostka.SZTUKA), 5.0, 3.0);
         Lodowka lodowka = new Lodowka();
         lodowka.dodajSkladnik(testowanySkladnik.getNazwa(), testowanySkladnik.getIloscS().getJednostka(), testowanySkladnik.getLimitOstrzezenie(), testowanySkladnik.getLimitKrytyczny());
         List<Skladnik> wynikZwroconaLista = lodowka.zwrocListeSkladnikow();
         Assert.assertTrue("lista zwrocona zawiera skladnik", wynikZwroconaLista.contains(testowanySkladnik));
         Assert.assertEquals("lista powinna zawierac dodane skladniki", 1, wynikZwroconaLista.size());
+    }
+
+    @Test
+    public void test_MozliweJestDodawanieTrzechSkladnikow(){
+        Lodowka lodowka = new Lodowka();
+        Skladnik testowanySkladnik1 = new Skladnik("Maslo", new IloscSkladnika(0.0, Jednostka.SZTUKA), 5.0, 3.0);
+        Skladnik testowanySkladnik2 = new Skladnik("Mleko", new IloscSkladnika(0.0, Jednostka.MILILITR), 500.0, 300.0);
+        Skladnik testowanySkladnik3 = new Skladnik("Cukier", new IloscSkladnika(0.0, Jednostka.GRAM), 1000.0, 500.0);
+        lodowka.dodajSkladnik(testowanySkladnik1.getNazwa(), testowanySkladnik1.getIloscS().getJednostka(), testowanySkladnik1.getLimitOstrzezenie(), testowanySkladnik1.getLimitKrytyczny());
+        lodowka.dodajSkladnik(testowanySkladnik2.getNazwa(), testowanySkladnik2.getIloscS().getJednostka(), testowanySkladnik2.getLimitOstrzezenie(), testowanySkladnik2.getLimitKrytyczny());
+        lodowka.dodajSkladnik(testowanySkladnik3.getNazwa(), testowanySkladnik3.getIloscS().getJednostka(), testowanySkladnik3.getLimitOstrzezenie(), testowanySkladnik3.getLimitKrytyczny());
+        List<Skladnik> wynikLista = lodowka.zwrocListeSkladnikow();
+        Assert.assertEquals("lista powinna zwierac trzy skladniki", 3, wynikLista.size());
+        Assert.assertTrue("lista zawiera składnik masło", wynikLista.contains(testowanySkladnik1));
+        Assert.assertTrue("lista zawiera składnik masło", wynikLista.contains(testowanySkladnik2));
+        Assert.assertTrue("lista zawiera składnik masło", wynikLista.contains(testowanySkladnik3));
+
+
+
     }
 
 
